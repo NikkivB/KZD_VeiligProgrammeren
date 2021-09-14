5fa04887d24cb656de6b4aad52270238cf158669
[NikkivB] <0316641@student.rocvantwente.nl>
2021-09-14 16:00:02 +0200

    create voorbeeld "Zeghetmaar"

---
+++ Zeghetmaar/Zeghetmaar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Zeghetmaar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection();
+            string cs = "Server=SQL6004.site4now.net;Database=DB_A2A0BC_vp;User Id=K0501;Password=ROCvT_K0501;";
+            conn.ConnectionString = cs;
+
+            conn.Open();
+
+        }
+    }
+}
