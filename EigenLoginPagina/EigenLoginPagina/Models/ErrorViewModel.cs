02f45c8b310bd8544f6e8aeba233f59eb16e3244
[NikkivB] <0316641@student.rocvantwente.nl>
2021-09-21 15:51:44 +0200

    create opdracht 2

---
+++ EigenLoginPagina/EigenLoginPagina/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EigenLoginPagina.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
